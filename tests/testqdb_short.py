0f18d9718a4fb9488e220dad9ee70700bc9f8177
Simon Krughoff <krughoff@astro.washington.edu>
2010-06-08 02:26:37 +0000

    added cases for galaxies and wds to testqdb_short.py

--- tests/testqdb_short.py
+++ tests/testqdb_short.py
@@ -2,8 +2,12 @@
 from lsst.sims.catalogs.generation.db import queryDB
 
 if __name__ == "__main__":
-  myqdb = queryDB.queryDB(chunksize=10)
+  myqdb = queryDB.queryDB(chunksize=10,objtype="STARS")
+  #myqdb = queryDB.queryDB(chunksize=10,objtype="WDSTARS")
+  #myqdb = queryDB.queryDB(chunksize=10,objtype="GALAXY")
   ic = myqdb.getInstanceCatalogById(85748128)
   print ic.metadata.parameters
-  print ic.dataArray
+  while len(ic.dataArray) > 0:
+    print ic.dataArray['id']
+    ic = myqdb.getNextChunk()
 
