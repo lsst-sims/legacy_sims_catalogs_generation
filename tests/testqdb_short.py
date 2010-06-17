bb024b2e8a5bcc7d6a6b9cdb15859393a6f475a9
Simon Krughoff <krughoff@astro.washington.edu>
2010-06-17 22:17:56 +0000

    New job state stuff and added stuff for raw catalog output

--- tests/testqdb_short.py
+++ tests/testqdb_short.py
@@ -2,12 +2,17 @@
 from lsst.sims.catalogs.generation.db import queryDB
 
 if __name__ == "__main__":
-  myqdb = queryDB.queryDB(chunksize=10,objtype="STARS")
+  myqdb = queryDB.queryDB(chunksize=100000,objtype="GALRAW")
   #myqdb = queryDB.queryDB(chunksize=10,objtype="WDSTARS")
   #myqdb = queryDB.queryDB(chunksize=10,objtype="GALAXY")
   ic = myqdb.getInstanceCatalogById(85748128)
+  ic.makeHelio()
   print ic.metadata.parameters
-  while len(ic.dataArray) > 0:
-    print ic.dataArray['id']
-    ic = myqdb.getNextChunk()
+  ic.writeCatalogData("test.dat", "GALRAW", newfile = True)
+  for i in range(len(ic.dataArray['id'])):
+    print len(ic.dataArray['id'])
+    ic.writeCatalogData("test.dat", "GALRAW", newfile = True)
+  #while len(ic.dataArray) > 0:
+  #  print ic.dataArray['id']
+  #  ic = myqdb.getNextChunk()
 
