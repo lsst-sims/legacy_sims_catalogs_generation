60922046c0d167efc0ba6ed928e948733188d4e5
Simon Krughoff <krughoff@astro.washington.edu>
2010-06-18 21:56:25 +0000

    Fix to try not to return Decimal types to Instance Catalog

--- tests/testqdb_short.py
+++ tests/testqdb_short.py
@@ -2,16 +2,15 @@
 from lsst.sims.catalogs.generation.db import queryDB
 
 if __name__ == "__main__":
-  myqdb = queryDB.queryDB(chunksize=100000,objtype="GALRAW")
+  #myqdb = queryDB.queryDB(chunksize=10,objtype="STARS")
   #myqdb = queryDB.queryDB(chunksize=10,objtype="WDSTARS")
-  #myqdb = queryDB.queryDB(chunksize=10,objtype="GALAXY")
+  myqdb = queryDB.queryDB(chunksize=10,objtype="GALAXY")
   ic = myqdb.getInstanceCatalogById(85748128)
   ic.makeHelio()
   print ic.metadata.parameters
-  ic.writeCatalogData("test.dat", "GALRAW", newfile = True)
+  ic.writeCatalogData("test.dat", None, newfile = True)
   for i in range(len(ic.dataArray['id'])):
-    print len(ic.dataArray['id'])
-    ic.writeCatalogData("test.dat", "GALRAW", newfile = True)
+    ic.writeCatalogData("test.dat", None, newfile = True)
   #while len(ic.dataArray) > 0:
   #  print ic.dataArray['id']
   #  ic = myqdb.getNextChunk()
