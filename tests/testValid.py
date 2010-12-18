11ff3c9b0039af14ed44ee7c64aefeab5efa9d15
Simon Krughoff <krughoff@astro.washington.edu>
2010-12-18 01:02:07 +0000

    Fixed format error for ra and dec offsets in trim files

--- tests/testValid.py
+++ tests/testValid.py
@@ -4,12 +4,13 @@ from lsst.sims.catalogs.generation.db import queryDB
 import lsst.sims.catalogs.measures.utils as mUtils
 
 if __name__ == "__main__":
-    csize = 10
+    csize = 1000
     objects = ("GALAXY","ALLSTARS","SSM")
     cattype = 'TRIM'
     for obj in objects:
+        print "Doing %s"%obj
         myqdb = queryDB.queryDB(chunksize=csize, objtype=obj)
-        ic = myqdb.getInstanceCatalogById(85748128)
+        ic = myqdb.getInstanceCatalogById(85748128, radiusdeg=0.01)
         mUtils.trimGeneration.derivedTrimMetadata(ic)
         ic.makeHelio()
         ic.makeTrimCoords()
@@ -21,4 +22,14 @@ if __name__ == "__main__":
         curMD.writeMetadata("hack_%s.dat"%(obj), cattype, myqdb.opsim,\
                 newfile=True) 
         ic.writeCatalogData("hack_%s.dat"%(obj), cattype)
+        ic = myqdb.getNextChunk()
+        cnum = 0
+        while ic is not None:
+        #    mUtils.trimGeneration.derivedTrimMetadata(ic)
+            print "Doing chunk %i"%cnum
+            ic.makeHelio()
+            ic.makeTrimCoords()
+            ic.writeCatalogData("hack_%s.dat"%(obj), cattype)
+            ic = myqdb.getNextChunk()
+            cnum += 1
 
