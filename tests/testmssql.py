4e76166f95ff5abe9525a52d1f2e84ca69f64c11
Simon Krughoff <krughoff@astro.washington.edu>
2011-02-25 20:09:47 +0000

    object enumeration update

--- tests/testmssql.py
+++ tests/testmssql.py
@@ -4,14 +4,17 @@ from lsst.sims.catalogs.generation.db import queryDB
 from lsst.sims.catalogs.measures.astrometry import Bbox
 
 if __name__ == "__main__":
-  csize = 10
-  myqdb = queryDB.queryDB(chunksize=csize,objtype="ALLSTARS")
-  ic = myqdb.getInstanceCatalogById(85748128)
-  print ic.metadata.parameters
-  print ic.dataArray.keys()
-  print len(ic.dataArray['ra'])
+  csize = 1000
+  myqdb = queryDB.queryDB(chunksize=csize,objtype="BHBSTARS")
+  ic = myqdb.getInstanceCatalogById(85748128, radiusdeg=2.1)
   ic.makeTrimCoords()
   ic.writeCatalogData("test.dat", "TRIM", newfile = True)
-  #while ic is not None:
-  #  print len(ic.dataArray['ra'])
-  #  ic = myqdb.getNextChunk()
+  ic = myqdb.getNextChunk()
+  cnum = 0
+  while ic is not None:
+    print "done with chunk %i"%cnum
+    ic.makeTrimCoords()
+    ic.writeCatalogData("test.dat", "TRIM", newfile = False)
+    ic = myqdb.getNextChunk()
+    cnum += 1
+
