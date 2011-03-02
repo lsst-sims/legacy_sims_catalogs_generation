200d0ea936dc7e11cf5cb7a3506b7637e6126499
Simon Krughoff <krughoff@astro.washington.edu>
2011-03-02 00:12:50 +0000

    Multiple changes to integrate the stored procedure call to the disk and bulge components of galaxies

--- tests/testmssql.py
+++ tests/testmssql.py
@@ -6,14 +6,14 @@ from lsst.sims.catalogs.generation.db import queryDB
 if __name__ == "__main__":
   csize = 10
   cattype = "TRIM"
-  myqdb = queryDB.queryDB(chunksize=csize,objtype="BHBSTARS")
-  ic = myqdb.getInstanceCatalogById(85748128, radiusdeg=2.1)
+  myqdb = queryDB.queryDB(chunksize=csize,objtype="GALAXY_BULGE")
+  ic = myqdb.getInstanceCatalogById(85748128, radiusdeg=0.1)
   mUtils.trimGeneration.derivedTrimMetadata(ic)
   ic.metadata.validateMetadata(cattype, myqdb.opsim) 
   ic.metadata.writeMetadata("test.dat", cattype, myqdb.opsim,\
          newfile=True) 
   ic.makeTrimCoords()
-  ic.writeCatalogData("test.dat", "TRIM", newfile = True)
+  ic.writeCatalogData("test.dat", "TRIM", newfile = False)
   ic = myqdb.getNextChunk()
   cnum = 0
   while ic is not None:
