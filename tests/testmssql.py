923fe37cf68081a9a2f0259ebb6f611670bbce53
Simon Krughoff <krughoff@astro.washington.edu>
2011-03-05 00:37:12 +0000

    Changes to speed up refactoring from ResultProxy to recarray-ish dataArray

--- tests/testmssql.py
+++ tests/testmssql.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 import pyoorb,math
+import time
 import lsst.sims.catalogs.measures.utils as mUtils
 from lsst.sims.catalogs.generation.db import queryDB
+from lsst.sims.catalogs.generation.db import jobDB
 
 if __name__ == "__main__":
-  csize = 1000
+  csize = 100000
   cattype = "TRIM"
-  myqdb = queryDB.queryDB(chunksize=csize,objtype="GALAXY_BULGE")
+  myqdb = queryDB.queryDB(chunksize=csize,objtype="ALLSTARS")
   ic = myqdb.getInstanceCatalogById(85748128, radiusdeg=0.1)
+  ic.makeTrimCoords()
   mUtils.trimGeneration.derivedTrimMetadata(ic)
   ic.metadata.validateMetadata(cattype, myqdb.opsim) 
   ic.metadata.writeMetadata("test.dat", cattype, myqdb.opsim,\
          newfile=True) 
-  ic.makeTrimCoords()
+  ic.validateData('TRIM')
   ic.writeCatalogData("test.dat", "TRIM", newfile = False)
   ic = myqdb.getNextChunk()
   cnum = 0
   while ic is not None:
-    print "done with chunk %i"%cnum
     ic.makeTrimCoords()
     ic.writeCatalogData("test.dat", "TRIM", newfile = False)
     ic = myqdb.getNextChunk()
