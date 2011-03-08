40dc02c2fc4e9a444d972817775c36e33fc51604
Simon Krughoff <krughoff@astro.washington.edu>
2011-03-08 00:44:50 +0000

    Added queries to bulge galaxies

--- tests/testmssql.py
+++ tests/testmssql.py
@@ -6,16 +6,21 @@ from lsst.sims.catalogs.generation.db import queryDB
 from lsst.sims.catalogs.generation.db import jobDB
 
 if __name__ == "__main__":
-  csize = 100000
+  csize = 10
   cattype = "TRIM"
-  myqdb = queryDB.queryDB(chunksize=csize,objtype="ALLSTARS")
-  ic = myqdb.getInstanceCatalogById(85748128, radiusdeg=0.1)
+  myqdb = queryDB.queryDB(chunksize=csize,objtype="GALAXY_BULGE")
+  ic = myqdb.getInstanceCatalogById(85748128, radiusdeg=.1)
   ic.makeTrimCoords()
   mUtils.trimGeneration.derivedTrimMetadata(ic)
   ic.metadata.validateMetadata(cattype, myqdb.opsim) 
   ic.metadata.writeMetadata("test.dat", cattype, myqdb.opsim,\
          newfile=True) 
   ic.validateData('TRIM')
+  keys = ic.dataArray.keys()
+  for row in xrange(len(ic.dataArray['raJ2000'])):
+    for k in keys:
+       print ic.dataArray[k][row],
+    print 
   ic.writeCatalogData("test.dat", "TRIM", newfile = False)
   ic = myqdb.getNextChunk()
   cnum = 0
