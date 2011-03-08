bb0997c265e1e4ee04f111be53ea1e913725d7b4
Simon Krughoff <krughoff@astro.washington.edu>
2011-03-08 22:15:28 +0000

    Minor changes to the test script

--- tests/testmssql.py
+++ tests/testmssql.py
@@ -8,25 +8,28 @@ from lsst.sims.catalogs.generation.db import jobDB
 if __name__ == "__main__":
   csize = 10
   cattype = "TRIM"
-  myqdb = queryDB.queryDB(chunksize=csize,objtype="ALLSTARS")
-  ic = myqdb.getInstanceCatalogById(85748128, radiusdeg=.1)
-  ic.makeTrimCoords()
-  mUtils.trimGeneration.derivedTrimMetadata(ic)
-  ic.metadata.validateMetadata(cattype, myqdb.opsim) 
-  ic.metadata.writeMetadata("test.dat", cattype, myqdb.opsim,\
-         newfile=True) 
-  ic.validateData('TRIM')
-  keys = ic.dataArray.keys()
-  for row in xrange(len(ic.dataArray['raJ2000'])):
-    for k in keys:
-       print ic.dataArray[k][row],
-    print 
-  ic.writeCatalogData("test.dat", "TRIM", newfile = False)
-  ic = myqdb.getNextChunk()
-  cnum = 0
-  while ic is not None:
-    ic.makeTrimCoords()
-    ic.writeCatalogData("test.dat", "TRIM", newfile = False)
-    ic = myqdb.getNextChunk()
-    cnum += 1
+  objtypes = ['ALLSTARS', 'GALAXY_BULGE', 'GALAXY_DISK', 'AGN']
+  for objtype in objtypes:
+      print "doing %s"%(objtype)
+      myqdb = queryDB.queryDB(chunksize=csize,objtype=objtype)
+      ic = myqdb.getInstanceCatalogById(85748128, radiusdeg=.1)
+      ic.makeTrimCoords()
+      mUtils.trimGeneration.derivedTrimMetadata(ic)
+      ic.metadata.validateMetadata(cattype, myqdb.opsim) 
+      ic.metadata.writeMetadata("test.dat", cattype, myqdb.opsim,\
+             newfile=True) 
+      ic.validateData('TRIM')
+      keys = ic.dataArray.keys()
+      for row in xrange(len(ic.dataArray['raJ2000'])):
+        for k in keys:
+           print ic.dataArray[k][row],
+        print 
+      ic.writeCatalogData("test.dat", "TRIM", newfile = False)
+      ic = myqdb.getNextChunk()
+      cnum = 0
+      #while ic is not None:
+      #  ic.makeTrimCoords()
+      #  ic.writeCatalogData("test.dat", "TRIM", newfile = False)
+      #  ic = myqdb.getNextChunk()
+      #  cnum += 1
 
