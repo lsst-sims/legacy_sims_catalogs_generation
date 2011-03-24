17aa54e540c855a7f1a5b9a0ea154f6223f77133
Simon Krughoff <krughoff@astro.washington.edu>
2011-03-24 22:16:32 +0000

    Attemts to get filter warnings

--- tests/testmssql.py
+++ tests/testmssql.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import pyoorb,math
-import time
+import exceptions,warnings
 import lsst.sims.catalogs.measures.utils as mUtils
 from lsst.sims.catalogs.generation.db import queryDB
 from lsst.sims.catalogs.generation.db import jobDB
@@ -8,14 +8,17 @@ from lsst.sims.catalogs.generation.db import jobDB
 if __name__ == "__main__":
   csize = 10000
   cattype = "TRIM"
-  objtypes = ['ALLSTARS', 'GALAXY_BULGE', 'GALAXY_DISK', 'AGN', 'SSM']
+  objtypes = ['ALLSTARS', 'GALAXY_BULGE', 'GALAXY_DISK', 'AGN', 'SSM',\
+          'GLENS', 'IMAGE']
+  varobj = ['ALLSTARS', 'AGN', 'IMAGE']
+  warnings.simplefilter('ignore', category=exceptions.UserWarning)
   for objtype in objtypes:
       outfile = "test_%s.dat"%(objtype)
       print "doing %s"%(objtype)
       myqdb = queryDB.queryDB(chunksize=csize,objtype=objtype)
       ic = myqdb.getInstanceCatalogById(85748128, radiusdeg=0.1)
       ic.makeTrimCoords()
-      if objtype in ['ALLSTARS', 'AGN']:
+      if objtype in varobj:
           ic.applyVariability()
       mUtils.trimGeneration.derivedTrimMetadata(ic)
       ic.metadata.validateMetadata(cattype, myqdb.opsim) 
@@ -27,7 +30,7 @@ if __name__ == "__main__":
       cnum = 0
       while ic is not None:
           ic.makeTrimCoords()
-          if objtype in ['ALLSTARS', 'AGN']:
+          if objtype in varobj:
               ic.applyVariability()
           ic.writeCatalogData(outfile, "TRIM", newfile = False)
           ic = myqdb.getNextChunk()
