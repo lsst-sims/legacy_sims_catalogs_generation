25afa4eb7afcc19f474de12e49e92309894de66c
Rob Gibson <rgibson@astro.washington.edu>
2011-03-29 00:01:58 +0000

    Test scripts.

---
+++ python/lsst/sims/catalogs/generation/jobAllocator/andyTest1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import pyoorb
+import numpy
+import math
+import copy
+import cPickle
+from lsst.sims.catalogs.generation.db import queryDB
+from lsst.sims.catalogs.measures.astrometry import Bbox
+import lsst.sims.catalogs.measures.utils as mUtils
+
+csize = 10
+myqdb = queryDB.queryDB(chunksize=csize, objtype="GALAXY_BULGE")
+ic = myqdb.getInstanceCatalogById(85748128)
+print ic.metadata.parameters
+curMD = copy.deepcopy(ic.metadata)
+mUtils.trimGeneration.derivedTrimMetadata(ic)
+print ic.metadata.parameters
+cPickle.dump(ic, open('test.pkl', 'w'))
+ic.makeHelio()
+ic.makeTrimCoords()
+ic.metadata.validateMetadata('TRIM', myqdb.opsim)
+print ic.dataArray
+ic.validateData('TRIM')
+ic.writeCatalogData('iCTest.txt', 'TRIM')
+ic.metadata.writeMetadata('iCTest.meta','TRIM', myqdb.opsim, newfile = True)
+
---
+++ python/lsst/sims/catalogs/generation/jobAllocator/andyTest3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import pyoorb, numpy, math, copy, cPickle, os, sys
+from lsst.sims.catalogs.generation.db import queryDB
+from lsst.sims.catalogs.measures.astrometry import Bbox
+import lsst.sims.catalogs.measures.utils as mUtils
+
+csize = 10
+myqdb = queryDB.queryDB(chunksize=csize, objtype=sys.argv[1])
+ic = myqdb.getInstanceCatalogById(85748128)
+print ic.metadata.parameters
+mUtils.trimGeneration.derivedTrimMetadata(ic)
+ic.calculateUnrefractedAltAz()
+print ic.metadata.parameters
+curMD = copy.deepcopy(ic.metadata)
+curMD.writeMetadata('testMetadata.dat', 'TRIM', myqdb.opsim, newfile=True)
+cPickle.dump(ic, open('test.pkl', 'w'))
+#ic.makeHelio()
+ic.makeTrimCoords()
+ic.metadata.validateMetadata('TRIM', myqdb.opsim)
+print ic.dataArray
+ic.validateData('TRIM')
+ic.writeCatalogData('iCTest.txt', 'TRIM')
+ic.metadata.writeMetadata('iCTest.meta','TRIM', myqdb.opsim, newfile = True)
+
