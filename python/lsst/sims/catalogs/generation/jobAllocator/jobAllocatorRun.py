e81b515ab22d38df5afe29130855222a15d2e84c
Rob Gibson <rgibson@astro.washington.edu>
2011-03-31 19:47:13 +0000

    Changes for InstanceCatalog interface, added variability support.

--- python/lsst/sims/catalogs/generation/jobAllocator/jobAllocatorRun.py
+++ python/lsst/sims/catalogs/generation/jobAllocator/jobAllocatorRun.py
@@ -2,6 +2,10 @@ import os, sys, cPickle, time
 import jobAllocatorStubs
 from lsst.sims.catalogs.generation.db import jobDB as jobDB
 
+# RRG:  Layering violation; should go in InstanceCat
+varTypes = ['ALLSTARS', 'BHBSTARS', 'MSRGBSTARS', 'WDSTARS',
+            'RRLYSTARS', 'AGN', 'IMAGE']
+
 if __name__ == '__main__':
     print 'Started with arg:', sys.argv[1], sys.argv[2], sys.argv[3]
     t0 = sys.argv[1].split('_')
@@ -11,7 +15,7 @@ if __name__ == '__main__':
     pickleFile = sys.argv[3]
     t0 = time.time()
     d.updateState(procId, 'jARRunning')
-    print 'Update to JARunning: %i' % (time.time() - t0)
+    print 'Update to JARunning: %7.3f' % (time.time() - t0)
     print 'JAR: State %s %s' % (
         procId, d.queryState(procId))
     print 'Started job: %s %s' % (
@@ -21,27 +25,34 @@ if __name__ == '__main__':
     print jobIdStr, 'Unpickling...'
     t0 = time.time()
     instanceCat = cPickle.load(open(pickleFile))
-    print 'pickle load: %i' % (time.time() - t0)
+    print 'pickle load: %7.3f' % (time.time() - t0)
 
     # Need to work with the catalog type to allow the possibility
     #  of writing out multiple types (loop not implemented yet)
     #  while allowing this code to choose the file names.
     dataFile = instanceCat.jobAllocatorDataFile
     catalogType = instanceCat.jobAllocatorCatalogType
+    objectType = instanceCat.jobAllocatorObjectType
 
-    t0 = time.time()
     # RRG:  I'm told makeHelio is no longer needed.
     #print jobIdStr, 'makeHelio...'
     #instanceCat.makeHelio()
+
+    t0 = time.time()
     print jobIdStr, 'makeTrimCoords...'
+    # RRG:  Layering violation; should go in InstanceCat
+    if objectType in varTypes: instanceCat.applyVariability()
+    print 'applyVariability: %7.3f' % (time.time() - t0)
+
+    t0 = time.time()
     instanceCat.makeTrimCoords()
     instanceCat.validateData(catalogType)
-    print 'makeHelio, makeTrimCoords, and validateData: %i' % (time.time() - t0)
+    print 'makeTrimCoords, and validateData: %7.3f' % (time.time() - t0)
 
     print jobIdStr, 'writeCatalogData... %s %s' % (dataFile, catalogType)
     t0 = time.time()
     instanceCat.writeCatalogData(dataFile, catalogType)
-    print 'writeCatalogData: %i' % (time.time() - t0)
+    print 'writeCatalogData: %7.3f' % (time.time() - t0)
 
     # Don't move the file; I want to find it and cat it to a trim file
     #t0 = 'mv %s /share/sdata1/rgibson/catOut/' % dataFile
@@ -50,5 +61,5 @@ if __name__ == '__main__':
     t0 = time.time()
     d.updateState(procId, 'JAFinished')
     print 'Updated state: %s to JAFinished' % procId
-    print 'Update to JAFinished: %i' % (time.time() - t0)
+    print 'Update to JAFinished: %7.3f' % (time.time() - t0)
 
