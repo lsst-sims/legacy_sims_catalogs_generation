9c87dd13b9d9d83413ff224d0a3416f112c60a7d
Rob Gibson <rgibson@astro.washington.edu>
2011-04-06 17:44:32 +0000

    QueryOnly mode.

--- python/lsst/sims/catalogs/generation/jobAllocator/test_obsjobAllocator.py
+++ python/lsst/sims/catalogs/generation/jobAllocator/test_obsjobAllocator.py
@@ -8,6 +8,13 @@ elif os.path.exists('/share/athena/share/sdata1/rgibson/'):
     workDir = '/share/athena/share/sdata1/rgibson/jobAllocator/'
 else:
     raise RuntimeError, '*** No place for workDir.'
-j = jobAllocator.JobAllocator(workDir=workDir, chunkSize=500000, maxCats=-1)
+print 'workDir:', workDir
+
+if sys.argv[2] = 'queryOnly':
+    q0 = True
+    print '*** DEBUG:  QueryOnly mode.'
+else: q0 = False
+
+j = jobAllocator.JobAllocator(workDir=workDir, chunkSize=500000, maxCats=-1, queryOnly=qO)
 # For some reason, need to use square brackets
 j.startCatalogs(['TRIM'], ['ALLSTARS', 'GLENS', 'IMAGE', 'EASTEREGGS', 'SSM', 'GALAXY_DISK', 'GALAXY_BULGE', 'AGN'], sys.argv[1])
