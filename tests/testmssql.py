e591e29251c575c18a003b673467b4ddb6cc4481
Simon Krughoff <krughoff@astro.washington.edu>
2011-04-07 23:40:38 +0000

    Changes to SSM objects and metadata

--- tests/testmssql.py
+++ tests/testmssql.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
-import pyoorb,math
+import pyoorb,math,sys
 import exceptions,warnings
 import lsst.sims.catalogs.measures.utils as mUtils
 from lsst.sims.catalogs.generation.db import queryDB
 from lsst.sims.catalogs.generation.db import jobDB
 
 if __name__ == "__main__":
-  csize = 10000
+  if len(sys.argv) == 2:
+      obsid = int(sys.argv[1])
+  else:
+      obsid = 85520357
+  csize = 100000
   cattype = "TRIM"
-  objtypes = ['ALLSTARS', 'GALAXY_BULGE', 'GALAXY_DISK', 'AGN', 'SSM',\
-          'GLENS', 'IMAGE', 'EASTEREGGS']
+  #objtypes = ['GALAXY_BULGE', 'GALAXY_DISK', 'AGN', 'ALLSTARS', 'SSM',\
+  #        'GLENS', 'IMAGE', 'EASTEREGGS']
+  objtypes = ['SSM']
   varobj = ['ALLSTARS', 'AGN', 'IMAGE']
   warnings.simplefilter('ignore', category=exceptions.UserWarning)
   for objtype in objtypes:
-      outfile = "test_%s.dat"%(objtype)
+      outfile = "test_%i_%s.dat"%(obsid,objtype)
       print "doing %s"%(objtype)
       myqdb = queryDB.queryDB(chunksize=csize,objtype=objtype)
-      ic = myqdb.getInstanceCatalogById(85520357, radiusdeg=0.5)
+      ic = myqdb.getInstanceCatalogById(obsid, radiusdeg=0.5)
       cnum = 0
       while ic is not None:
           ic.makeTrimCoords()
