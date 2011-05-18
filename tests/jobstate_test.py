3568683d687d54cdef572b609e6aa34eb0ac4148
Simon Krughoff <krughoff@astro.washington.edu>
2011-05-18 00:16:59 +0000

    Modified job state API

--- tests/jobstate_test.py
+++ tests/jobstate_test.py
@@ -11,3 +11,10 @@ js.updateState("mykey","now my value")
 print js.queryState("mykey")
 js.updateState("newkey", "my new key")
 print js.queryState("newkey")
+print "***Printing all keys***"
+states = js.showStates()
+for k in states.keys():
+  print "%s %s"%(k, states[k])
+jids = js.getJobIdsByOwner('anon')
+for jid in jids:
+  print jid.getId()
