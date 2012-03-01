d507f1d4ff7da48ce19ae1719bac5d1ec2868182
Rob Gibson <rgibson@astro.washington.edu>
2012-03-01 18:22:59 +0000

    Updated to add time stamps.

--- python/lsst/sims/catalogs/generation/utils/testThrottleObsList.py
+++ python/lsst/sims/catalogs/generation/utils/testThrottleObsList.py
@@ -4,22 +4,16 @@ import os, sys, time
 from lsst.sims.catalogs.generation.db import jobDB
 import throttleUtils
 
-maxNumJobs = 100
+maxNumJobs = int(sys.argv[1])
 waitTime = 5
-
 radDeg = 2.1
-#obsList = [
-#    '88533589', '88544863', '88545027', '88572282', '88572405',
-#    '88625722', '88625768', '88625873', '88626439', '88646885',
-#    '88646916', '88646953', '88646972', '88673409', '88673551',
-#    '88687053', '88689380', '88689479', '88691372', '88691519',
-#    '88691756', '88691823', '88702909', '88702909', '88725297',
-#    '88725438', '88730520', '88747522', '88747566', '88747628',
-#    '88747751', '88767581', '88775449', '88775554', '88775587',
-#    '88808278'
-#]
 
-#obsList = ['88533589']
+possibleTestModeStr = ''
+if len(sys.argv) > 2:
+    if sys.argv[2].lower() == 'testmode':
+        possibleTestModeStr = ' testMode'
+        print '>>> Using test mode.'
+        
 obsList = []
 f = open('inList.txt', 'r')
 for line in f:
@@ -45,12 +39,14 @@ for i in range(len(obsList)):
     # For now, call addJob() before actually starting the job,
     #  because there could be a race condition if addJob()
     #  and removeJob() are called simultaneously.
-    throttleUtils.addJob(executionDBManager, jobId, obsList[i])
+    t0 = int(time.time())
+    t1 = '%s_%i' % (obsList[i], t0)
+    throttleUtils.addJob(executionDBManager, jobId, t1)
 
     #jobIdPlusI = jobId + '_' + str(i)
     cwd0 = os.getcwd()
     f0 = open('/share/pogo3/rgibson/testFramework011312/sTScripts/tempST%s.csh' % jobId, 'w')
-    f0.write('#!/bin/csh\n#PBS -N %i_sT%s\n#PBS -l walltime=23:59:59\n#PBS -e /share/pogo3/rgibson/testFramework011312/out/sT%s.err\n#PBS -o /share/pogo3/rgibson/testFramework011312/out/sT%s.out\n\ncd %s\nsource setupOldAthena.csh\npython ./testThrottleObsListRun.py %s %s %s %s\necho Finished.' % (i, jobId, jobId, jobId, cwd0, nFN, jobId, obsList[i], str(radDeg)))
+    f0.write('#!/bin/csh\n#PBS -N %i_sT%s\n#PBS -l qos=astro,walltime=47:59:59,nodes=1:ppn=1\n#PBS -e /share/pogo3/rgibson/testFramework011312/out/sT%s.err\n#PBS -o /share/pogo3/rgibson/testFramework011312/out/sT%s.out\n\ncd %s\nsource setupOldAthena.csh\npython ./testThrottleObsListRun.py %s %s %s %s%s\necho Finished.' % (i, jobId, jobId, jobId, cwd0, nFN, jobId, obsList[i], str(radDeg), possibleTestModeStr))
     f0.close()
     # Use this from a compute node
     t0 = 'ssh minerva0 "(cd %s; /opt/torque/bin/qsub ./sTScripts/tempST%s.csh)"' % (cwd0, jobId)
