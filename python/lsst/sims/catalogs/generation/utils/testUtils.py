2193dbeb7c7afaf91907b3904d4cad5778925e8b
Simon Krughoff <krughoff@uw.edu>
2013-08-05 17:57:37 -0700

    Added some testing with a fake star database

---
+++ python/lsst/sims/catalogs/generation/utils/testUtils.py
@@ -0,0 +1,68 @@
+import sqlite3
+from numpy.random import random, seed
+from sqlite3 import dbapi2 as sqlite
+import numpy
+
+from lsst.sims.catalogs.generation.db import DBObject, ObservationMetaData
+
+class myTestStars(DBObject):
+    objid = 'teststars'
+    tableid = 'stars'
+    idColKey = 'id'
+    #Make this implausibly large?  
+    appendint = 1023
+    dbAddress = 'sqlite:///testDatabase.db'
+    raColName = 'ra'
+    decColName = 'decl'
+    spatialModel = 'POINT'
+    columns = [('id', None, int),
+               ('ra', None),
+               ('decl', None),
+               ('umag', None),
+               ('gmag', None),
+               ('rmag', None),
+               ('imag', None),
+               ('zmag', None),
+               ('ymag', None),
+               ('mag_norm', None)]
+
+def sampleSphere(size):
+    #From Shao 1996: "Spherical Sampling by Archimedes' Theorem"
+    ra = random(size)*2.*numpy.pi
+    z = random(size)*2. - 1.
+    dec = numpy.arccos(z) - numpy.pi/2.
+    return ra, dec
+
+def makeTestDB(size=1000):
+    conn = sqlite3.connect('testDatabase.db')
+    c = conn.cursor()
+    try:
+        c.execute('''CREATE TABLE stars
+                     (id int, ra real, decl real, umag real, gmag real, rmag real, 
+                     imag real, zmag real, ymag real, mag_norm real)''')
+        conn.commit()
+    except:
+        raise RuntimeError("Error creating database.")
+    seed(1)
+    ra, dec = sampleSphere(size)
+    #Typical colors
+    umg = 1.5
+    gmr = 0.65
+    rmi = 1.0
+    imz = 0.45
+    zmy = 0.3
+    mag_norm = random(size)*6. + 18.
+    #assume mag norm is g-band (which is close to true)
+    umag = mag_norm + umg
+    gmag = mag_norm
+    rmag = gmag - gmr
+    imag = rmag - rmi
+    zmag = imag - imz
+    ymag = zmag -zmy
+    for i in xrange(size):
+        c.execute('''INSERT INTO stars VALUES (%i, %f, %f, %f, %f, %f, %f, %f, %f, %f)'''%\
+                  (i, numpy.degrees(ra[i]), numpy.degrees(dec[i]), umag[i], gmag[i], rmag[i], imag[i], zmag[i], ymag[i], mag_norm[i]))
+    c.execute('''CREATE INDEX ra_idx ON stars (ra)''')
+    c.execute('''CREATE INDEX dec_idx ON stars (decl)''')
+    conn.commit()
+    conn.close()
