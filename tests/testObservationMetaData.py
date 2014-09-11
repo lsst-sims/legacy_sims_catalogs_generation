278200ce075cc15b725f5e8bcd315828d4a14079
danielsf <scott.f.daniel@gmail.com>
2014-09-11 14:37:22 -0700

    added unit test to test that observationMetaData is passing values correctly

--- tests/testObservationMetaData.py
+++ tests/testObservationMetaData.py
@@ -3,8 +3,11 @@ import unittest
 import lsst.utils.tests as utilsTests
 from lsst.sims.catalogs.generation.db import ObservationMetaData
 
-
-class ObservationMetaDataTestCase(unittest.TestCase):
+class ObservationMetaDataTest(unittest.TestCase):
+    """
+    This class will test that ObservationMetaData correctly assigns
+    and returns its class variables (UnrefractedRA, UnrefractedDec, etc.)
+    """
     def testInit(self):
         m5tuple = (0,1,2,3)
         m5float = 25.
@@ -36,13 +39,27 @@ class ObservationMetaDataTestCase(unittest.TestCase):
         obsMD = ObservationMetaData(box_bounds=box_bounds, m5=m5float)
         self.assertAlmostEqual(obsMD.m5('u'),m5float,10)
         self.assertAlmostEqual(obsMD.m5('sally'),m5float,10)
+         
+    def testScalars(self):
+        mjd = 5120.0
+        RA = 1.5
+        Dec = -1.1
+        RotSkyPos = -0.2
+        
+        testObsMD = ObservationMetaData(mjd=mjd, UnrefractedRA=RA,
+            UnrefractedDec=Dec, RotSkyPos=RotSkyPos)
+        
+        self.assertAlmostEqual(testObsMD.mjd,5120.0,10)
+        self.assertAlmostEqual(testObsMD.UnrefractedRA,1.5,10)
+        self.assertAlmostEqual(testObsMD.UnrefractedDec,-1.1,10)
+        self.assertAlmostEqual(testObsMD.RotSkyPos,-0.2,10)
         
 
 def suite():
     """Returns a suite containing all the test cases in this module."""
     utilsTests.init()
     suites = []
-    suites += unittest.makeSuite(ObservationMetaDataTestCase)
+    suites += unittest.makeSuite(ObservationMetaDataTest)
 
     return unittest.TestSuite(suites)
 
