933854c0c4529a4195aa8513e234fb571df0a719
danielsf <scott.f.daniel@gmail.com>
2014-09-24 09:47:26 -0700

    Gave the test databases associated with testDbobject.py less generic names so there is less risk of differenty unittests deleting each other's databases when they are run in parallel.
    This required creating daughter classes of myTestStars and myTestGals that
    would point to the less generic databases.

--- tests/testDbobject.py
+++ tests/testDbobject.py
@@ -6,6 +6,7 @@ import lsst.utils.tests as utilsTests
 from lsst.sims.catalogs.generation.db import DBObject, fileDBObject,ObservationMetaData
 from lsst.sims.catalogs.generation.db.observationMetadataUtils import haversine
 import lsst.sims.catalogs.generation.utils.testUtils as tu
+from lsst.sims.catalogs.generation.utils.testUtils import myTestStars, myTestGals
 
 # This test should be expanded to cover more of the framework
 # I have filed CATSIM-90 for this.
@@ -16,10 +17,10 @@ def createNonsenseDB():
     This will be used to make sure that circ_bounds and box_bounds yield the points
     they are supposed to.
     """
-    if os.path.exists('NonsenseDB.db'):
-        os.unlink('NonsenseDB.db')
+    if os.path.exists('testDbobjectNonsenseDB.db'):
+        os.unlink('testDbobjectNonsenseDB.db')
     
-    conn = sqlite3.connect('NonsenseDB.db')
+    conn = sqlite3.connect('testDbobjectNonsenseDB.db')
     c = conn.cursor()
     try:
         c.execute('''CREATE TABLE test (id int, ra real, dec real, mag real)''')
@@ -42,7 +43,7 @@ class myNonsenseDB(DBObject):
     objid = 'Nonsense'
     tableid = 'test'
     idColKey = 'NonsenseId' 
-    dbAddress = 'sqlite:///NonsenseDB.db'
+    dbAddress = 'sqlite:///testDbobjectNonsenseDB.db'
     raColName = 'ra'
     decColName = 'dec'
     columns = [('NonsenseId', 'id', int),
@@ -61,17 +62,22 @@ class myNonsenseFileDB(fileDBObject):
                ('NonsenseDecJ2000', 'dec*%f'%(numpy.pi/180.)),
                ('NonsenseMag','mag',float)]
 
+class testDbobjectTestStars(myTestStars):
+    objid = 'testDbobjectTeststars'
+    dbAddress = 'sqlite:///testDbobjectDatabase.db'
 
-
+class testDbobjectTestGalaxies(myTestGals):
+    objid = 'testDbobjectTestgals'
+    dbAddress = 'sqlite:///testDbobjectDatabase.db'
 
 class DBObjectTestCase(unittest.TestCase):
   
     #Delete the test database if it exists and start fresh.
-    if os.path.exists('testDatabase.db'):
+    if os.path.exists('testDbobjectDatabase.db'):
         print "deleting database"
-        os.unlink('testDatabase.db')
-    tu.makeStarTestDB(size=100000, seedVal=1)
-    tu.makeGalTestDB(size=100000, seedVal=1)
+        os.unlink('testDbobjectDatabase.db')
+    tu.makeStarTestDB(filename='testDbobjectDatabase.db',size=100000, seedVal=1)
+    tu.makeGalTestDB(filename='testDbobjectDatabase.db',size=100000, seedVal=1)
     createNonsenseDB()
     obsMd = ObservationMetaData(circ_bounds=dict(ra=210., dec=-60, radius=1.75),
                                      mjd=52000., bandpassName='r')
@@ -81,8 +87,8 @@ class DBObjectTestCase(unittest.TestCase):
     
     """
     baselineData will store another copy of the data that should be stored in
-    NonsenseDB.db.  This will give us something to test database queries against
-    when we ask for all of the objects within a certain box_bounds or circ_bounds.
+    testDbobjectNonsenseDB.db.  This will give us something to test database queries 
+    against when we ask for all of the objects within a certain box_bounds or circ_bounds.
     """
     baselineData=None
     for line in inFile:
@@ -103,15 +109,15 @@ class DBObjectTestCase(unittest.TestCase):
         self.assertAlmostEqual(self.obsMd.mjd, 52000., 6)
     
     def testDbObj(self):
-        mystars = DBObject.from_objid('teststars')
-        mygals = DBObject.from_objid('testgals')
+        mystars = DBObject.from_objid('testDbobjectTeststars')
+        mygals = DBObject.from_objid('testDbobjectTestgals')
         result = mystars.query_columns(obs_metadata=self.obsMd)
         tu.writeResult(result, "/dev/null")
         result = mystars.query_columns(obs_metadata=self.obsMd)
         tu.writeResult(result, "/dev/null")
 
     def testRealQueryConstraints(self):
-        mystars = DBObject.from_objid('teststars')
+        mystars = DBObject.from_objid('testDbobjectTeststars')
         mycolumns = ['id','raJ2000','decJ2000','umag','gmag','rmag','imag','zmag','ymag']
         
         #recall that ra and dec are stored in degrees in the data base
@@ -306,7 +312,7 @@ class DBObjectTestCase(unittest.TestCase):
         Test that a query with a specified chunk_size does, in fact, return chunks of that size
         """
         
-        mystars = DBObject.from_objid('teststars')
+        mystars = DBObject.from_objid('testDbobjectTeststars')
         mycolumns = ['id','raJ2000','decJ2000','umag','gmag']
         myquery = mystars.query_columns(colnames = mycolumns, chunk_size = 1000)
         
@@ -321,33 +327,33 @@ class DBObjectTestCase(unittest.TestCase):
         variables of DBObject
         """
         
-        mystars = DBObject.from_objid('teststars')
+        mystars = DBObject.from_objid('testDbobjectTeststars')
         myNonsense = DBObject.from_objid('Nonsense')
-        mygalaxies = DBObject.from_objid('testgals')
+        mygalaxies = DBObject.from_objid('testDbobjectTestgals')
         
         self.assertEqual(mystars.raColName,'ra')
         self.assertEqual(mystars.decColName,'decl')
         self.assertEqual(mystars.idColKey,'id')
-        self.assertEqual(mystars.dbAddress,'sqlite:///testDatabase.db')
+        self.assertEqual(mystars.dbAddress,'sqlite:///testDbobjectDatabase.db')
         self.assertEqual(mystars.appendint, 1023)
         self.assertEqual(mystars.tableid,'stars')
         self.assertFalse(hasattr(mystars,'spatialModel'))
-        self.assertEqual(mystars.objid,'teststars')
+        self.assertEqual(mystars.objid,'testDbobjectTeststars')
         
         self.assertEqual(mygalaxies.raColName,'ra')
         self.assertEqual(mygalaxies.decColName,'decl')
         self.assertEqual(mygalaxies.idColKey,'id')
-        self.assertEqual(mygalaxies.dbAddress,'sqlite:///testDatabase.db')
+        self.assertEqual(mygalaxies.dbAddress,'sqlite:///testDbobjectDatabase.db')
         self.assertEqual(mygalaxies.appendint, 1022)
         self.assertEqual(mygalaxies.tableid,'galaxies')
         self.assertTrue(hasattr(mygalaxies,'spatialModel'))
         self.assertEqual(mygalaxies.spatialModel,'SERSIC2D')
-        self.assertEqual(mygalaxies.objid,'testgals')
+        self.assertEqual(mygalaxies.objid,'testDbobjectTestgals')
         
         self.assertEqual(myNonsense.raColName,'ra')
         self.assertEqual(myNonsense.decColName,'dec')
         self.assertEqual(myNonsense.idColKey,'NonsenseId')
-        self.assertEqual(myNonsense.dbAddress,'sqlite:///NonsenseDB.db')
+        self.assertEqual(myNonsense.dbAddress,'sqlite:///testDbobjectNonsenseDB.db')
         self.assertFalse(hasattr(myNonsense,'appendint'))
         self.assertEqual(myNonsense.tableid,'test')
         self.assertFalse(hasattr(myNonsense,'spatialModel'))
@@ -355,6 +361,8 @@ class DBObjectTestCase(unittest.TestCase):
         
         self.assertTrue('teststars' in DBObject.registry)
         self.assertTrue('testgals' in DBObject.registry)
+        self.assertTrue('testDbobjectTeststars' in DBObject.registry)
+        self.assertTrue('testDbobjectTestgals' in DBObject.registry)
         self.assertTrue('Nonsense' in DBObject.registry)
        
         colsShouldBe = [('id',None,int),('raJ2000','ra*%f'%(numpy.pi/180.)),
@@ -423,8 +431,8 @@ class fileDBObjectTestCase(unittest.TestCase):
     
     """
     baselineData will store another copy of the data that should be stored in
-    NonsenseDB.db.  This will give us something to test database queries against
-    when we ask for all of the objects within a certain box_bounds or circ_bounds.
+    testDbobjectNonsenseDB.db.  This will give us something to test database queries 
+    against when we ask for all of the objects within a certain box_bounds or circ_bounds.
     """
     baselineData=None
     for line in inFile:
