6a01a4b95ec5abc4b40305477f55a8820efc7c68
danielsf <scott.f.daniel@gmail.com>
2015-08-24 12:41:13 -0700

    add unit test to verify that CompoundCatalogDBObject correctly handles ObservationMetaData

--- tests/testCompoundCatalogDBObject.py
+++ tests/testCompoundCatalogDBObject.py
@@ -5,6 +5,7 @@ import os
 import lsst.utils.tests as utilsTests
 from lsst.utils import getPackageDir
 
+from lsst.sims.utils import ObservationMetaData
 from lsst.sims.catalogs.generation.db import fileDBObject, \
                                       CompoundCatalogDBObject, \
                                       CatalogDBObject
@@ -263,12 +264,139 @@ class CompoundCatalogDBObjectTestCase(unittest.TestCase):
                                                     decimal=6)
 
 
+class testStarDB1(CatalogDBObject):
+    tableid = 'test'
+    raColName = 'ra'
+    decColName = 'dec'
+    idColKey = 'id'
+    objid = 'testStar1'
+    columns = [('id', None),
+               ('raJ2000', 'ra'),
+               ('decJ2000', 'dec'),
+               ('magMod', 'mag')]
+
+
+class testStarDB2(CatalogDBObject):
+    tableid = 'test'
+    raColName = 'ra'
+    decColName = 'dec'
+    idColKey = 'id'
+    objid = 'testStar2'
+    columns = [('id', None),
+               ('raJ2000', '2.0*ra'),
+               ('decJ2000', '2.0*dec'),
+               ('magMod', '2.0*mag')]
+
+
+class CompoundWithObsMetaData(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.baseDir = os.path.join(getPackageDir('sims_catalogs_generation'),
+                                   'tests', 'scratchSpace')
+
+
+        cls.textFileName = os.path.join(cls.baseDir, 'compound_obs_metadata_text_data.txt')
+
+        numpy.random.seed(42)
+        nSamples = 100
+        raList = numpy.random.random_sample(nSamples)*360.0
+        decList = numpy.random.random_sample(nSamples)*180.0 - 90.0
+        magList = numpy.random.random_sample(nSamples)*15.0 + 7.0
+
+        dtype = numpy.dtype([
+                            ('ra', numpy.float),
+                            ('dec', numpy.float),
+                            ('mag', numpy.float)
+                            ])
+
+        cls.controlArray = numpy.rec.fromrecords([
+                                                  (r, d, m) \
+                                                  for r, d, m in \
+                                                  zip(raList, decList, magList)
+                                                  ], dtype=dtype)
+
+        dbDtype = numpy.dtype([
+                              ('id', numpy.int),
+                              ('ra', numpy.float),
+                              ('dec', numpy.float),
+                              ('mag', numpy.float)
+                              ])
+
+        if os.path.exists(cls.textFileName):
+            os.unlink(cls.textFileName)
+
+        with open(cls.textFileName,'w') as output:
+            output.write('# id ra dec mag\n')
+            for ix, (r, d, m) in enumerate(zip(raList, decList, magList)):
+                output.write('%d %.20f %.20f %.20f\n' % (ix, r, d, m))
+
+        cls.dbName = os.path.join(cls.baseDir,'compound_obs_metadata_db.db')
+
+        if os.path.exists(cls.dbName):
+            os.unlink(cls.dbName)
+
+        fdbo = fileDBObject(cls.textFileName, runtable='test',
+                            database=cls.dbName, dtype=dbDtype,
+                            idColKey='id')
+
+
+    def testObsMetaData(self):
+        """
+        Test that CompoundCatalogDBObject can handle ObservationMetaData
+        properly
+        """
+
+        obs = ObservationMetaData(unrefractedRA = 180.0,
+                                  unrefractedDec = 0.0,
+                                  boundType = 'box',
+                                  boundLength = (80.0, 25.0))
+
+        db1 = testStarDB1(database=self.dbName, driver='sqlite')
+        db2 = testStarDB2(database=self.dbName, driver='sqlite')
+
+        compoundDb = CompoundCatalogDBObject([db1, db2])
+
+        colnames = ['testStar1_id',
+                    'testStar1_raJ2000', 'testStar1_decJ2000', 'testStar1_magMod',
+                    'testStar2_raJ2000', 'testStar2_decJ2000', 'testStar2_magMod']
+
+        results = compoundDb.query_columns(colnames=colnames,
+                                           obs_metadata=obs)
+
+        good_rows = []
+        for chunk in results:
+            for line in chunk:
+                ix = line['id']
+                good_rows.append(ix)
+                self.assertAlmostEqual(line['testStar1_raJ2000'], self.controlArray['ra'][ix], 10)
+                self.assertAlmostEqual(line['testStar1_decJ2000'], self.controlArray['dec'][ix], 10)
+                self.assertAlmostEqual(line['testStar1_magMod'], self.controlArray['mag'][ix], 10)
+                self.assertAlmostEqual(line['testStar2_raJ2000'], 2.0*self.controlArray['ra'][ix], 10)
+                self.assertAlmostEqual(line['testStar2_decJ2000'], 2.0*self.controlArray['dec'][ix], 10)
+                self.assertAlmostEqual(line['testStar2_magMod'], 2.0*self.controlArray['mag'][ix], 10)
+                self.assertTrue(self.controlArray['ra'][ix]>100.0)
+                self.assertTrue(self.controlArray['ra'][ix]<260.0)
+                self.assertTrue(self.controlArray['dec'][ix]>-25.0)
+                self.assertTrue(self.controlArray['dec'][ix]<25.0)
+
+        bad_rows = [ix for ix in range(self.controlArray.shape[0]) if ix not in good_rows]
+
+        in_bounds = [rr>100.0 and rr<260.0 and dd>-25.0 and dd<25.0 \
+                      for (rr, dd) in \
+                      zip(self.controlArray['ra'][bad_rows], self.controlArray['dec'][bad_rows])]
+
+        self.assertFalse(True in in_bounds)
+        self.assertTrue(len(good_rows)>0)
+        self.assertTrue(len(bad_rows)>0)
+
 
 def suite():
     """Returns a suite containing all the test cases in this module."""
     utilsTests.init()
     suites = []
     suites += unittest.makeSuite(CompoundCatalogDBObjectTestCase)
+    suites += unittest.makeSuite(CompoundWithObsMetaData)
 
 
     return unittest.TestSuite(suites)
